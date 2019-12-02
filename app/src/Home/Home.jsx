853d7173840a2a1296689a82d9aaf81456145793
traysi may <teh@raptor.pizza>
2019-12-02 00:38:50 -0700

    more dots

--- app/src/Home/Home.jsx
+++ app/src/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 
 export const Home = () => {
   const canvas = useRef();
+  const clicked = useRef(false);
   const width = window.innerWidth;
   const height = window.innerHeight;
 
@@ -9,12 +10,14 @@ export const Home = () => {
     const c = canvas.current;
     const ctx = c.getContext("2d");
     const centerX = c.width / 2;
-    const centerY = c.height / 2;
-    const radius = 70;
+    const centerY = c.height / 4;
+
+    c.addEventListener("click", () => (clicked.current = true));
+    c.addEventListener("touchstart", () => (clicked.current = true));
 
     const drawCircle = (x, y, o1, o2) => {
       ctx.beginPath();
-      ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+      ctx.arc(x, y, Math.floor(Math.random() * 100), 0, 2 * Math.PI, false);
       ctx.fillStyle = "red";
       ctx.globalAlpha = o1;
       ctx.fill();
@@ -25,12 +28,13 @@ export const Home = () => {
     };
 
     let counter = 0;
+    let req;
     const animate = () => {
-      requestAnimationFrame(animate);
+      req = requestAnimationFrame(animate);
       counter++;
       const o1 = Math.random();
       const o2 = Math.random();
-      if (counter < 100) {
+      if (counter < 1000 && !clicked.current) {
         drawCircle(centerX, centerY, o1, o2);
       } else {
         c.style.position = "absolute";
@@ -44,6 +48,8 @@ export const Home = () => {
     };
 
     animate();
+
+    return () => cancelAnimationFrame(req);
   }, []);
   return <canvas ref={canvas} width={width} height={height}></canvas>;
 };
