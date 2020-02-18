8d466bdf8fcb1bdac32836c63d938fefa9b10ecf
traysimay <teh@raptor.pizza>
2020-02-18 09:43:32 -0700

    bg bug

--- app/src/ChapterTwo/ChapterTwo.jsx
+++ app/src/ChapterTwo/ChapterTwo.jsx
@@ -11,34 +11,50 @@ import animationData from "./animations/smiler.json";
 const ChapterTwo = ({ history }) => {
   const [phase, setPhase] = useState(1);
   const isNDA = authenticationService.currentNDAValue;
+
+  const blackBG = () => {
+    document.body.style.background = "black";
+    document.documentElement.style.background = "black";
+  };
+
+  const redBG = () => {
+    document.body.style.background = "#ff4747";
+    document.documentElement.style.background = "#ff4747";
+  };
+
   useEffect(() => {
     if (localStorage.getItem("phase")) {
       setPhase(parseInt(localStorage.getItem("phase")));
     }
-    document.body.style.background = "black";
-    document.documentElement.style.background = "black";
+    redBG();
     return () => {
-      document.body.style.background = "black";
-      document.documentElement.style.background = "black";
+      blackBG();
     };
   }, []);
-  if (isNDA)
+  if (isNDA) {
+    blackBG();
     return (
-      <div style={{ padding: "1rem" }}>
+      <div style={{ padding: "0 1.6rem" }}>
         <Link
           to={"/rsvp"}
           style={{
-            fontSize: "7rem",
+            fontSize: "6rem",
             width: "90%",
-            margin: "0 1rem",
-            color: "white"
+            margin: "auto",
+            display: "block",
+            color: "white",
+            margin: "0 0 1.5rem"
           }}
         >
           come to the party
         </Link>
-        <Lottie options={{ loop: true, autoplay: true, animationData }} />
+        <Lottie
+          width="50%"
+          options={{ loop: true, autoplay: true, animationData }}
+        />
       </div>
     );
+  }
   return (
     <div>
       {phase === 1 && <Phase1 setPhase={setPhase} />}
